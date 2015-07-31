825fd5f42dd40267da12424a791b634208a690f1
alex gherega <alex.gherega@gmail.com>
2015-07-31 21:21:35 +0300

    Adding first web service: a memoservice

---
+++ memoservice/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to memoservice
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ memoservice/test/memoservice/core_test.clj
@@ -0,0 +1,7 @@
+(ns memoservice.core-test
+  (:require [clojure.test :refer :all]
+            [memoservice.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
