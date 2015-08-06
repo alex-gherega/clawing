5d27a7f8e5979591362fb8afa965197a64e74690
alex gherega <alex.gherega@gmail.com>
2015-08-06 07:16:04 +0300

    Addign more functinoality to memoservice

--- memoservice/src/memoservice/core.clj
+++ memoservice/src/memoservice/core.clj
@@ -1,3 +1,22 @@
-(ns memoservice.core)
+(ns memoservice.core
+  (:require [clojure.data.json :as json]))
 
 (def *current-idx* (atom 1))
+
+(defn- read-questions [path]
+  (-> path
+      slurp
+      json/read-json
+      :questions))
+
+(def *vip-idxs*
+  (-> "lib/very-imp-q.json" read-questions atom))
+
+(def *mip-idxs*
+  (-> "lib/most-imp-q.json" read-questions atom))
+
+(defn reset-vip []
+  (reset! *vip-idxs* (-> "lib/very-imp-q.json" read-questions)))
+
+(defn reset-mip []
+  (reset! *mip-idxs* (-> "lib/most-imp-q.json" read-questions)))
--- memoservice/src/memoservice/handler.clj
+++ memoservice/src/memoservice/handler.clj
@@ -5,24 +5,44 @@
             [ring.util.response :as rur]
             [memoservice.core :as memoco]))
 
-(defn- spit-url-str []
-  (let [cix @memoco/*current-idx*]
-    (swap! memoco/*current-idx* inc)
-    (str "http://e-drpciv.ro/intrebare/" cix)))
+(defn- spit-url-str [a swap-fn a-fn]
+  (let [cix @a]
+    (swap! a swap-fn)
+    (str "http://e-drpciv.ro/intrebare/" (a-fn cix))))
 
-(defn- spit-main-html []
+(defn- spit-main-html [a swap-fn a-fn]
   (str "<iframe src='"
-       (spit-url-str)
+       (spit-url-str a swap-fn a-fn)
        "' "
        "height=100% "
        "width=100%>"
        "</iframe>"))
 
 (defroutes app-routes
-  (GET "/" [] (spit-main-html))
+  (GET "/" [] (spit-main-html memoco/*current-idx* inc identity))
+
+  (GET "/reverse" [] (spit-main-html memoco/*current-idx* dec identity))
+
+  (GET "/vip" [] (spit-main-html memoco/*vip-idxs*
+                                 rest
+                                 first))
+
+  (GET "/mip" [] (spit-main-html memoco/*mip-idxs*
+                                 rest
+                                 first))
+
+  (GET "/reset/vip" []
+       (memoco/reset-vip)
+       (rur/redirect "/vip"))
+
+  (GET "/reset/mip" []
+       (memoco/reset-mip)
+       (rur/redirect "/mip"))
+
   (GET "/reset/:val" [val]
        (reset! memoco/*current-idx* (read-string val))
        (rur/redirect "/"))
+
   (route/not-found "Not Found"))
 
 (def app
